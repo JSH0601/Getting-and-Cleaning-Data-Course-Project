207421fd8741d0e1ed16ccf914835bfd04c6da1c
JSH <Jesssritchie@gmail.com>
2016-03-12 23:47:53 -0500

    Bug fixes and final file added

--- run_analysis.R
+++ run_analysis.R
@@ -6,23 +6,21 @@
 ##also, verify that you have dplyr installed and it is loaded by using
 ##"library(dplyr"), and reshape2 by "library(reshape2)"
 
-##Set working directory to the UCI HAR Dataset folder before beginning
-
 ##imports features.txt (the labels for all test variables)
-features <- read.table("features.txt")
+features <- read.table('./UCI_HAR_Dataset/features.txt')
 
 ##imports activity_labels.txt (the activity labels for all recorded activities)
-activitylabels <- read.table("activity_labels.txt")
+activitylabels <- read.table('./UCI_HAR_Dataset/activity_labels.txt')
 
 ##imports all test data
-testsubject <- read.table('./test/subject_test.txt')
-testactivity <- read.table('./test/y_test.txt')
-testdata <- read.table('./test/X_test.txt')
+testsubject <- read.table('./UCI_HAR_Dataset/test/subject_test.txt')
+testactivity <- read.table('./UCI_HAR_Dataset/test/y_test.txt')
+testdata <- read.table('./UCI_HAR_Dataset/test/X_test.txt')
 
 ##imports all training data
-trainingsubject <- read.table('./train/subject_train.txt')
-trainingactivity <- read.table('./train/y_train.txt')
-trainingdata <- read.table('./train/X_train.txt')
+trainingsubject <- read.table('./UCI_HAR_Dataset/train/subject_train.txt')
+trainingactivity <- read.table('./UCI_HAR_Dataset/train/y_train.txt')
+trainingdata <- read.table('./UCI_HAR_Dataset/train/X_train.txt')
 
 ##merges the activity values from each data set with the activity labels
 ##and converts the original variables into one column
@@ -47,23 +45,18 @@ names(alltrainingdata) <- allvariablenames
 allstudydata <- rbind(alltestdata, alltrainingdata)
 
 ##extracts mean and standard deviation columns from the combined data
-substudydata <- subset(allstudydata, select = grep("mean|std", names(allstudydata)))
+substudydata <- subset(allstudydata, select = grep("mean|std|subject|activity", colnames(allstudydata)))
 substudydata <- substudydata[,-grep("Freq", colnames(substudydata))]
 
 ##renames the new extracted table with appropriate variable names
-names(substudydata) <- sub("Acc", "accelerometer", names(substudydata))
-names(substudydata) <- sub("Gyro", "gyroscope", names(substudydata))
-names(substudydata) <- sub("mag", "magnitude", names(substudydata))
-names(substudydata) <- c(substudydata[,1:2], sub("t", "timed", names(substudydata[,3:40]), substudydata[,41:66]))
-names(substudydata) <- c(substudydata[,1:40], sub("f", "fastfourier",names(substudydata[,41:66])))
-names(substudydata) <- c(substudydata[,1:60], sub("Body", "", names(substudydata[,61:66])))
-names(substudydata) <- sub("-", "", names(substudydata))
-names(substudydata) <- sub("()", "", names(substudydata))
+names(substudydata) <- c(names(substudydata[,1:2]), sub("t", "timed", names(substudydata[,3:42])), names(substudydata[,43:68]))
+names(substudydata) <- c(names(substudydata[,1:42]), sub("f", "fastfourier", names(substudydata[,43:68])))
+names(substudydata) <- sub("std", "deviation", names(substudydata))
 
 ##creates a second data set to display the average of each variable for each
 ##activity and each subject
-studydatameans <- melt(substudydata, id= substudydata[,1:2], measure.vars = substudydata[,3:66])
-studydatameans <- dcast(studydatameans, activity ~ variable,mean)
+studydatameans <- melt(substudydata, id= names(substudydata[,1:2]))
+studydatameans <- dcast(studydatameans, subject + activity ~ variable,mean)
 
 ##makes final data set available in Global Environment
 studydatameans <<- studydatameans
